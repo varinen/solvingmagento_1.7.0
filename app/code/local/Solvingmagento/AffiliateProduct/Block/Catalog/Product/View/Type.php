fa2420edc2a818b207c296b89dc1dd38e391b403
Oleg Ishenko <o.ishenko@k-newmedia.de>
2013-09-09 15:55:42 +0200

    Continuing work on the module

---
+++ app/code/local/Solvingmagento/AffiliateProduct/Block/Catalog/Product/View/Type.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Solvingmagento_AffiliateProduct type view class
+ *
+ * PHP version 5.3
+ *
+ * @category Solvingmagento
+ * @package Solvingmagento_AffiliateProduct
+ * @author Oleg Ishenko <oleg.ishenko@solvingmagento.com>
+ * @copyright 2013 Oleg Ishenko
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version GIT: <0.1.0>
+ * @link http://www.solvingmagento.com/
+ *
+ */
+
+/** Solvingmagento_AffiliateProduct_Block_Catalog_Product_View_Type
+ *
+ * @category Solvingmagento
+ * @package Solvingmagento_AffiliateProduct
+ *
+ * @author Oleg Ishenko <oleg.ishenko@solvingmagento.com>
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version Release: <package_version>
+ * @link http://www.solvingmagento.com/
+ */
+class Solvingmagento_AffiliateProduct_Block_Catalog_Product_View_Type extends Mage_Catalog_Block_Product_View_Type_Virtual
+{
+
+}
