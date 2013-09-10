75cc4a1dfbf799416e83c48d5c9886f53d18cd02
Oleg Ishenko <o.ishenko@k-newmedia.de>
2013-09-10 18:28:02 +0200

    Added a compare helper rewrite

---
+++ app/code/local/Solvingmagento/AffiliateProduct/Helper/Compare.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * A Mage_Catalog_Helper_Product_Compare helper extension
+ *
+ * PHP version 5.3
+ *
+ * @category Solvingmagento
+ * @package  Solvingmagento_AffiliateProduct
+ * @author   Magento Core Team <core@magentocommerce.com>
+ * @author   Oleg Ishenko <oleg.ishenko@solvingmagento.com>
+ * @license  http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version  GIT: <0.1.0>
+ * @link     http://www.solvingmagento.com/
+ *
+ */
+
+/** Solvingmagento_AffiliateProduct_Helper_Compare
+ *
+ * @category Solvingmagento
+ * @package  Solvingmagento_AffiliateProduct
+ *
+ * @author  Magento Core Team <core@magentocommerce.com>
+ * @author  Oleg Ishenko <oleg.ishenko@solvingmagento.com>
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version Release: <package_version>
+ * @link    http://www.solvingmagento.com/
+ */
+class Solvingmagento_AffiliateProduct_Helper_Compare extends Mage_Catalog_Helper_Product_Compare
+{
+    /**
+     * Retrive add to cart url
+     *
+     * @param Mage_Catalog_Model_Product $product
+     * @return string
+     */
+    public function getAddToCartUrl($product)
+    {
+        if ($product->getTypeId() === Solvingmagento_AffiliateProduct_Model_Product_Type::TYPE_AFFILIATE) {
+            return Mage::helper('solvingmagento_affiliateproduct')->getRedirectUrl($product);
+        }
+
+        $beforeCompareUrl = Mage::getSingleton('catalog/session')->getBeforeCompareUrl();
+        $params = array(
+            'product'=>$product->getId(),
+            Mage_Core_Controller_Front_Action::PARAM_NAME_URL_ENCODED => $this->getEncodedUrl($beforeCompareUrl)
+        );
+
+        return $this->_getUrl('checkout/cart/add', $params);
+    }
+
+}
