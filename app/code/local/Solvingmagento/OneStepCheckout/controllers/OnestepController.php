e53dd0aa4ee316b679e86d2346fa0d33ea4cdffa
Oleg Ishenko <o.ishenko@k-newmedia.de>
2014-01-30 16:02:42 +0100

    Initial commit for the one step checkout module

---
+++ app/code/local/Solvingmagento/OneStepCheckout/controllers/OnestepController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Solvingmagento_OneStepCheckout controller class
+ *
+ * PHP version 5.3
+ *
+ * @category  Solvingmagento
+ * @package   Solvingmagento_OneStepCheckout
+ * @author    Oleg Ishenko <oleg.ishenko@solvingmagento.com>
+ * @copyright 2014 Oleg Ishenko
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version   GIT: <0.1.0>
+ * @link      http://www.solvingmagento.com/
+ *
+ */
+
+/** Solvingmagento_OneStepCheckout_OnestepController
+ *
+ * @category Solvingmagento
+ * @package  Solvingmagento_OneStepCheckout
+ *
+ * @author  Oleg Ishenko <oleg.ishenko@solvingmagento.com>
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @version Release: <package_version>
+ * @link    http://www.solvingmagento.com/
+ */
+class Mage_Checkout_OnepageController extends Mage_Checkout_Controller_Action
+{
+
+}
