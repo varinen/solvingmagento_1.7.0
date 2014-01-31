baa46b5626a8d2a4d03db4d422d27689a43eb3bf
Oleg Ishenko <o.ishenko@k-newmedia.de>
2014-01-31 17:45:47 +0100

    Added onestep controller

--- app/code/local/Solvingmagento/OneStepCheckout/controllers/OnestepController.php
+++ app/code/local/Solvingmagento/OneStepCheckout/controllers/OnestepController.php
@@ -24,7 +24,76 @@
  * @version Release: <package_version>
  * @link    http://www.solvingmagento.com/
  */
-class Mage_Checkout_OnepageController extends Mage_Checkout_Controller_Action
-{
+$controllerPath = Mage::getBaseDir('code') . DS . 'core' . DS . 'Mage' . DS . 'Checkout' . DS . 'controllers' . DS . 'OnepageController.php';
+require_once $controllerPath;
 
+class Solvingmagento_OneStepCheckout_OnestepController extends Mage_Checkout_OnepageController
+{
+    /**
+     * Validate ajax request and redirect on failure
+     *
+     * @return bool
+     */
+    protected function _expireAjax()
+    {
+        if (!$this->getOnestep()->getQuote()->hasItems()
+            || $this->getOnestep()->getQuote()->getHasError()
+            || $this->getOnestep()->getQuote()->getIsMultiShipping()) {
+            $this->_ajaxRedirectResponse();
+            return true;
+        }
+        $action = $this->getRequest()->getActionName();
+        if (Mage::getSingleton('checkout/session')->getCartWasUpdated(true)
+            && !in_array($action, array('index', 'progress'))) {
+            $this->_ajaxRedirectResponse();
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get one page checkout model
+     *
+     * @return Solvingmagento_OneStepCheckout_Model_Type_Onestep
+     */
+    public function getOnestep()
+    {
+        return Mage::getSingleton('slvmto_onestepc/type_onestep');
+    }
+
+    /**
+     * Checkout page
+     */
+    public function indexAction()
+    {
+        if (!Mage::helper('slvmto_onestepc')->oneStepCheckoutEnabled()) {
+            Mage::getSingleton('checkout/session')
+                ->addError($this->__('One Step checkout is disabled.'));
+            $this->_redirect('checkout/cart');
+            return;
+        }
+        $quote = $this->getOnestep()->getQuote();
+        if (!$quote->hasItems() || $quote->getHasError()) {
+            $this->_redirect('checkout/cart');
+            return;
+        }
+        if (!$quote->validateMinimumAmount()) {
+            $error = Mage::getStoreConfig('sales/minimum_order/error_message') ?
+                Mage::getStoreConfig('sales/minimum_order/error_message') :
+                Mage::helper('checkout')->__('Subtotal must exceed minimum order amount');
+
+            Mage::getSingleton('checkout/session')->addError($error);
+            $this->_redirect('checkout/cart');
+            return;
+        }
+        Mage::getSingleton('checkout/session')->setCartWasUpdated(false);
+        Mage::getSingleton('customer/session')
+            ->setBeforeAuthUrl(Mage::getUrl('*/*/*', array('_secure'=>true)));
+        $this->getOnestep()->initCheckout();
+        $this->loadLayout();
+        $this->_initLayoutMessages('customer/session');
+        $this->getLayout()->getBlock('head')->setTitle($this->__('Checkout'));
+        $this->renderLayout();
+    }
 }
